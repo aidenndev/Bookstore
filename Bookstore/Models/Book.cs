56a6940882fc2c2b44cc6fd89df7b4fd7aaa2fae
aidenndev <aiden.nguyen.dev@gmail.com>
2023-03-29 20:51:31 +1030

    Added Book & Customer Models

---
+++ Bookstore/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }  
+        public DateTime ReservedDate { get; set; }
+        public Customer? Customer { get; set; }
+    }
+}
---
+++ Bookstore/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
